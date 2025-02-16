73c337f1b521b3eabcb6c54bf1715acef185425e
Tiaguitoo09 <martinezbeltransantiago9@gmail.com>
2025-02-16 15:12:55 -0500

    Setup Proyect

---
+++ src/main/java/UtadeoProgamacionAvanzada/LeyDeCiclos/LeyDeCiclosApplication.java
@@ -0,0 +1,11 @@
+package UtadeoProgamacionAvanzada.LeyDeCiclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyDeCiclosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyDeCiclosApplication.class, args);
+    }
+}
---
+++ src/test/java/UtadeoProgamacionAvanzada/LeyDeCiclos/LeyDeCiclosApplicationTests.java
@@ -0,0 +1,13 @@
+package UtadeoProgamacionAvanzada.LeyDeCiclos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyDeCiclosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
